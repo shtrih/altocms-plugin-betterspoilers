0d1d74ec1942f147242ee807c37a1d00b3a458c8
Dmitry M <s@hitagi.ru>
2015-09-25 17:27:59 +0400

    Благодаря https://github.com/altocms/altocms/pull/734 можно избавиться от костыля.

--- templates/skin/default/assets/js/betterspoilers.js
+++ templates/skin/default/assets/js/betterspoilers.js
@@ -1,19 +1,12 @@
 ;(function ($, document) {
     $(document).ready(function () {
-        // сраный костыль для сраного iCheck, который навязан всем чекбоксам без разбора
-        setTimeout(function () {
-            if ($.fn.iCheck) {
-                $('.betterspoiler > [class^="icheckbox_"] input, .spoiler > [class^="icheckbox_"] input').iCheck('destroy');
+        $('.betterspoiler > input ~ a:last-of-type').on('click', function () {
+            var self = $(this);
+            self.prevAll('input[type=checkbox]').trigger('click');
 
-                $('.betterspoiler > input ~ a:last-of-type').on('click', function () {
-                    var self = $(this);
-                    self.prevAll('input[type=checkbox]').trigger('click');
+            $.scrollTo(self.parent(), 500, {interrupt: true});
 
-                    $.scrollTo(self.parent(), 500, {interrupt: true});
-
-                    return false;
-                });
-            }
-        }, 1000);
+            return false;
+        });
     });
 }(jQuery, document));
